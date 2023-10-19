d2e17ac3947223e05bfbdc3d64dfc01b90100aee
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-10-19 04:30:26 -0400

    WIP

--- test/runErc20ServiceTest.js
+++ test/runErc20ServiceTest.js
@@ -1,20 +1,17 @@
 require("dotenv").config();
-let DEBUG_MODE = true;
 
 const { ethers } = require('ethers')
-const { AlphaRouterServiceDebug } = require('../lib/debug/AlphaRouterServiceDebug')
-const { AlphaRouterService, ERC20Services } = require('../lib/prod/AlphaRouterService');
+const { ERC20Services } = require('../lib/prod/AlphaRouterService');
 
 const GOERLI_INFURA_TEST_URL = process.env.GOERLI_INFURA_TEST_URL
 const CHAIN_ID = parseInt(process.env.GOERLI_CHAIN_ID)
 const WALLET_SECRET = process.env.WALLET_SECRET
 const WETH_ADDRESS = process.env.GOERLI_WETH
-const UNI_ADDRESS = process.env.GOERLI_UNI
+const USDC_ADDRESS = process.env.GOERLI_USDC
 
 let erc20Services = new ERC20Services(ethers, GOERLI_INFURA_TEST_URL, CHAIN_ID)
 // let provider = new ethers.providers.JsonRpcProvider(GOERLI_INFURA_TEST_URL)
 // let provider = erc20Services.provider
-let ARS = DEBUG_MODE ? new AlphaRouterServiceDebug( erc20Services ) : new AlphaRouterService( erc20Services );
 
 tokenAmountToWeiTest = ( _amount, _decimals ) => {
     let wei = erc20Services.tokenAmtToBigint( _amount, _decimals )
@@ -24,53 +21,55 @@ tokenAmountToWeiTest = ( _amount, _decimals ) => {
 tokenAddrToBigIntAmtTest = async( _tokenAddress, _amount ) => {
     let wei = await erc20Services.tokenAddrToBigintAmt( _tokenAddress, _amount )
     let tokenContract = erc20Services.getERC20Contract( _tokenAddress );
-    let tokenName = await tokenContract.name()
-    console.log(`tokenAddrToBigIntAmtTest: ${_amount} ${tokenName} token(s) equals ${ wei.toString()} bigInt(s)`);
+    let nameSymbol = await erc20Services.getContractNameSymbol(tokenContract)
+    console.log(`tokenAddrToBigIntAmtTest: ${_amount} ${nameSymbol} token(s) equals ${ wei.toString()} bigInt(s)`);
 }
 
 tokenContractAmtToBigIntTest = async( _tokenAddress, _amount ) => {
     let tokenContract = erc20Services.getERC20Contract( _tokenAddress );
-    let tokenName = await tokenContract.name()
+    let nameSymbol = await erc20Services.getContractNameSymbol(tokenContract)
     let wei = await erc20Services.tokenContractAmtToBigInt( tokenContract, _amount )
-    console.log(`tokenContractAmtToBigIntTest: ${_amount} ${tokenName} token(s) equals ${ wei.toString()} bigInt(s)`);
+    console.log(`tokenContractAmtToBigIntTest: ${_amount} ${nameSymbol} token(s) equals ${ wei.toString()} bigInt(s)`);
 }
 
 bigIntToTokenAmtTest = async( _weiAmount, _decimals ) => {
     let tokenAmt = erc20Services.bigIntToTokenAmt( _weiAmount, _decimals )
-    console.log(`bigIntToTokenAmtTest: ${_weiAmount} bigInt(s) equals ${ tokenAmt} token(s)`);
+    console.log(`bigIntToTokenAmtTest: ${_weiAmount} bigInt(s) with ${_decimals} decimals equals ${ tokenAmt} token(s)`);
 }
 
 bigIntToTokenAddrAmtTest = async( _tokenAddress, _amount ) => {
     let tokenContract = erc20Services.getERC20Contract( _tokenAddress );
-    let tokenName = await tokenContract.name()
+    let nameSymbol = await erc20Services.getContractNameSymbol(tokenContract)
     let tokenAmt = await erc20Services.bigIntToTokenAddrAmt( _tokenAddress, _amount )
-    console.log(`bigIntToTokenContractAmtTest: ${_amount} bigInt(s) equals ${ tokenAmt.toString()} ${tokenName} token(s)`);
+    console.log(`bigIntToTokenContractAmtTest: ${_amount} bigInt(s) equals ${ tokenAmt.toString()} ${nameSymbol} token(s)`);
 }
 
 bigIntToTokenContractAmtTest = async( _tokenAddress, _amount ) => {
     let tokenContract = erc20Services.getERC20Contract( _tokenAddress );
-    let tokenName = await tokenContract.name()
+    let nameSymbol = await erc20Services.getContractNameSymbol(tokenContract)
     let tokenAmt = await erc20Services.bigIntToTokenContractAmt( tokenContract, _amount )
-    console.log(`bigIntToTokenContractAmtTest: ${_amount} bigInt(s) equals ${ tokenAmt.toString()} ${tokenName} token(s)`);
+    console.log(`bigIntToTokenContractAmtTest: ${_amount} bigInt(s) equals ${ tokenAmt.toString()} ${nameSymbol} token(s)`);
 }
 
 main = async( ) => {
     // wethToEthByAddressTest(WALLET_SECRET, WETH_ADDRESS, 1)
-    // wallet = erc20Services.Wallet(WALLET_SECRET)
-    console.log("------------------------------------------------------------------------------------------------")
+    wallet = erc20Services.Wallet(WALLET_SECRET)
+    console.log("----------------------------------------------------------------------------------------------------------------")
     tokenAmountToWeiTest( 1, 18 )
-    console.log("------------------------------------------------------------------------------------------------")
+    console.log("----------------------------------------------------------------------------------------------------------------")
     await tokenAddrToBigIntAmtTest(WETH_ADDRESS, 1)
-    console.log("------------------------------------------------------------------------------------------------")
+    console.log("----------------------------------------------------------------------------------------------------------------")
     await tokenContractAmtToBigIntTest(WETH_ADDRESS, "1") 
-    console.log("------------------------------------------------------------------------------------------------")
+    console.log("----------------------------------------------------------------------------------------------------------------")
     await bigIntToTokenAddrAmtTest(WETH_ADDRESS , 456)
-    console.log("------------------------------------------------------------------------------------------------")
-    await bigIntToTokenContractAmtTest(WETH_ADDRESS , 123)
-    console.log("------------------------------------------------------------------------------------------------")
+    console.log("----------------------------------------------------------------------------------------------------------------")
+    await bigIntToTokenContractAmtTest(USDC_ADDRESS , 123)
+    console.log("----------------------------------------------------------------------------------------------------------------")
     bigIntToTokenAmtTest("12345678901234567890123456789",6)
-    console.log("------------------------------------------------------------------------------------------------")
-    console.log("TESTING FINISHED: EXITING")
+    console.log("----------------------------------------------------------------------------------------------------------------")
+    await bigIntToTokenAmtTest("12345678900000000000000000", 18)
+    console.log("----------------------------------------------------------------------------------------------------------------")
+     console.log("TESTING FINISHED: EXITING")
 }
 
 main()
