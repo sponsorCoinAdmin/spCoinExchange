c3a59efcf20a86ecd90995be979d2f2ff42f962d
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-10-21 21:31:01 -0400

    WIP

--- test/runWethEthConvertTest.js
+++ test/runWethEthConvertTest.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const { ethers, JsonRpcProvider } = require('ethers')
+const { ethers } = require('ethers')
 const { ERC20Services, DebugErc20Services } = require('../lib/prod/debugErc20Services');
 
 const GOERLI_INFURA_TEST_URL = process.env.GOERLI_INFURA_TEST_URL
@@ -12,9 +12,10 @@ const WETH_ADDRESS = process.env.GOERLI_WETH
 let erc20Services = new ERC20Services(ethers, GOERLI_INFURA_TEST_URL, CHAIN_ID)
 let debugErc20Services = new DebugErc20Services(ethers, GOERLI_INFURA_TEST_URL, CHAIN_ID)
 
+DEBUG_MODE=true
+let DBM = DEBUG_MODE ? debugErc20Services : erc20Services;
+
 async function wrapEthTest( _ethAmountInWei ) {
-    // Ensure ethAmountInWei is a string
-    let ethAmountInWei = "" + _ethAmountInWei
     let provider = new ethers.providers.JsonRpcProvider(GOERLI_INFURA_TEST_URL);
     let wallet = new ethers.Wallet(WALLET_SECRET, provider);
     const wethContract = new ethers.Contract(WETH_ADDRESS, WETH_ABI, wallet);
@@ -25,37 +26,31 @@ async function wrapEthTest( _ethAmountInWei ) {
 }
 
 wrapEthAmtByAddressTest = async(_wallet, _wethAddress, _ethAmount) => {
-    // debugErc20Services.getBalanceOf( _wallet.address, _wethAddress )
-    await erc20Services.wrapEthAmtByAddress(_wallet, _wethAddress, _ethAmount)
+    await DBM.wrapEthAmtByAddress(_wallet, _wethAddress, _ethAmount)
 }
 
 unwrapEthAmtByAddressTest = async(_wallet, _wethAddress, _ethAmount) => {
-    debugErc20Services.getBalanceOf( _wallet.address, _wethAddress )
-    await erc20Services.unwrapEthAmtByAddress(_wallet, _wethAddress, _ethAmount)
+    await DBM.unwrapEthAmtByAddress(_wallet, _wethAddress, _ethAmount)
 }
 
 wrapEthAmtByContractTest = async(_wallet, _wethAddress, _ethAmount) => {
-    let wethContract = erc20Services.getWETHContract(_wethAddress );
-    // debugErc20Services.getContractBalanceOf( _wallet, _wethAddress )
-    await erc20Services.wrapEthAmtByContract(_wallet, wethContract, _ethAmount)
+    let wethContract = DBM.getWETHContract(_wethAddress );
+    await DBM.wrapEthAmtByContract(_wallet, wethContract, _ethAmount)
 }
 
 unwrapEthAmtByContractTest = async(_wallet, _wethAddress, _ethAmount) => {
-    let wethContract = erc20Services.getWETHContract(_wethAddress );
-    // debugErc20Services.getContractBalanceOf( _wallet, _wethAddress )
-    await erc20Services.unwrapEthAmtByContract(_wallet, wethContract, _ethAmount)
+    let wethContract = DBM.getWETHContract(_wethAddress );
+    await DBM.unwrapEthAmtByContract(_wallet, wethContract, _ethAmount)
 }
 
 main = async( ) => {
-    // wethToEthByAddressTest(WALLET_SECRET, WETH_ADDRESS, 1)
-
-    let wallet = erc20Services.Wallet(WALLET_SECRET)
+    let wallet = DBM.Wallet(WALLET_SECRET)
     let ethAmount = 0.0123
     console.log("------------------------------------------------------------------------------------------------")
-    await wrapEthAmtByAddressTest(wallet, WETH_ADDRESS, ethAmount)
-    console.log("------------------------------------------------------------------------------------------------")
-    await unwrapEthAmtByAddressTest(wallet, WETH_ADDRESS, ethAmount)
-    console.log("------------------------------------------------------------------------------------------------")
+    // await wrapEthAmtByAddressTest(wallet, WETH_ADDRESS, ethAmount)
+    // console.log("------------------------------------------------------------------------------------------------")
+    // await unwrapEthAmtByAddressTest(wallet, WETH_ADDRESS, ethAmount)
+    // console.log("------------------------------------------------------------------------------------------------")
     await wrapEthAmtByContractTest(wallet, WETH_ADDRESS, ethAmount)
     console.log("------------------------------------------------------------------------------------------------")
     await unwrapEthAmtByContractTest(wallet, WETH_ADDRESS, ethAmount)
